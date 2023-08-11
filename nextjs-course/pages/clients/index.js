42f81c0a4f1bc607a4144d4536950274a6f866e6
gsuvodip <gsuvodip.webdev@gmail.com>
2023-08-11 17:49:37 +0530

    alternate Link href

--- nextjs-course/pages/clients/index.js
+++ nextjs-course/pages/clients/index.js
@@ -12,7 +12,15 @@ function ClientPage() {
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
-            <Link href={`clients/${client.id}`}>{client.name}</Link>
+            {/* <Link href={`clients/${client.id}`}>{client.name}</Link> */}
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
           </li>
         ))}
       </ul>
