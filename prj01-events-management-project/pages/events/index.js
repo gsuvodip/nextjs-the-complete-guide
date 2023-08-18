b2b7cf844e06899b9208d0bca69daa03927bfa05
gsuvodip <gsuvodip.webdev@gmail.com>
2023-08-18 19:53:51 +0530

    using SSR

--- prj01-events-management-project/pages/events/index.js
+++ prj01-events-management-project/pages/events/index.js
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-utils";
 
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -18,6 +18,16 @@ function AllEventsPage() {
       <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+  };
 }
 
 export default AllEventsPage;
