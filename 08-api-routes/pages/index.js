2202cb56b6c5a0fbd0f18abea3577ac601a95bc9
gsuvodip <gsuvodip.webdev@gmail.com>
2023-09-22 16:47:10 +0530

    sending requests to api routes

--- 08-api-routes/pages/index.js
+++ 08-api-routes/pages/index.js
@@ -10,13 +10,24 @@ function HomePage() {
     const enteredEmail = emailInputRef.current.value;
     const enteredFeedback = feedbackInputRef.current.value;
 
-    fetch(); // {email: 'test@test.com', text: 'soem feedback text'}
+    // {email: 'test@test.com', text: 'soem feedback text'}
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   return (
     <div>
       <h1>The Home Page</h1>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
           <input type="email" id="email" ref={emailInputRef} />
